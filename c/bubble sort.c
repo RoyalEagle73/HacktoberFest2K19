c3ae4726b08ac9b4c169e1fbca9b3289e4195d1f
prabhatap20 <56965104+prabhatap20@users.noreply.github.com>
2019-10-24 19:55:09 +0530

    Add files via upload

---
+++ c/bubble sort.c
@@ -0,0 +1,34 @@
+//bubble sort
+#include<stdio.h>
+int main()
+{
+  int array[100], n, i, j, swap;
+
+  printf("Enter number of elements\n");
+  scanf("%d", &n);
+
+  printf("Enter %d integers\n", n);
+
+  for (i = 0; i < n; i++)
+    scanf("%d", &array[i]);
+
+  for (i = 0 ; i < n - 1; i++)
+  {
+    for (j = 0 ; j < n - i - 1; j++)
+    {
+      if (array[j] > array[j+1]) /* For increasing order use < */
+      {
+        swap       = array[j];
+        array[j]   = array[j+1];
+        array[j+1] = swap;
+      }
+    }
+  }
+
+  printf("Sorted list in ascending order:\n");
+
+  for (i = 0; i < n; i++)
+     printf("%d\n", array[i]);
+
+  return 0;
+}
