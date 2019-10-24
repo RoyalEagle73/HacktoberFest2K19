5fabbc0f0a1483b81f856f88ebd2fa7450334e06
Balram5762 <56962170+Balram5762@users.noreply.github.com>
2019-10-24 19:57:52 +0530

    astrik
    pattern astrik

---
+++ c/asterik pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+for(i=1;i<=4;i++)
+        {
+            for(j=1;j<=4;j++)
+            {
+                if(i==1||i==4||j==1||j==4)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+            printf("\n");
+        }
+   return 0;
+}
